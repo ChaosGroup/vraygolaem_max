43b3ad74ec37558038a7232b0bc259a1402c9fcf
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-02-20 15:23:48 +0200

    misc: sdk version

--- cmake_vrayGolaemMax.bat
+++ cmake_vrayGolaemMax.bat
@@ -13,7 +13,7 @@ if %MAX_VERSION% LSS 2018 set TOOLSET=-T v110
 "C:/Program Files/CMake/bin/cmake.exe" ^
 -G"Visual Studio 15 2017 Win64" %TOOLSET% %* ^
 -DCMAKE_INSTALL_PREFIX="C:/Program Files/Autodesk/3ds Max %MAX_VERSION%" ^
--DGOLAEMDEVKIT_ROOTDIR="C:/programs/golaem/Maya/%MAYA_VERSION%/6.0.1/devkit" ^
+-DGOLAEMDEVKIT_ROOTDIR="C:/programs/golaem/Maya/%MAYA_VERSION%/6.2.3/devkit" ^
 -D3DSMAXSDK_VERSION=%MAX_VERSION% ^
 -D3DSMAXSDK_ROOTDIR="C:/Program Files/Autodesk/3ds Max %MAX_VERSION%" ^
 -D3DSMAXSDK_INCDIR="%KDRIVE2%/3dsmax/maxsdk%MAX_VERSION%/include" ^
