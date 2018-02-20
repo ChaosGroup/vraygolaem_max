c68e88f94f5fd04f66e6a3e60b045c4fe3fe7f16
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-02-20 16:48:12 +0200

    fix: better install

--- cmake_vrayGolaemMax.bat
+++ cmake_vrayGolaemMax.bat
@@ -12,7 +12,7 @@ if %MAX_VERSION% LSS 2018 set TOOLSET=-T v110
 
 "C:/Program Files/CMake/bin/cmake.exe" ^
 -G"Visual Studio 15 2017 Win64" %TOOLSET% %* ^
--DCMAKE_INSTALL_PREFIX="C:/Program Files/Autodesk/3ds Max %MAX_VERSION%" ^
+-DCMAKE_INSTALL_PREFIX="C:/install/golaem" ^
 -DGOLAEMDEVKIT_ROOTDIR="C:/programs/golaem/Maya/%MAYA_VERSION%/6.2.3/devkit" ^
 -D3DSMAXSDK_VERSION=%MAX_VERSION% ^
 -D3DSMAXSDK_ROOTDIR="C:/Program Files/Autodesk/3ds Max %MAX_VERSION%" ^
